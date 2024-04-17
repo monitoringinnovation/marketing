c4019e90bf960c99f6046658577c44a53d5bd085
Davidh120 <david.antonio12@hotmail.com>
2024-04-17 14:41:01 -0500

    feat: Create project

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Marketing",
+
+    'summary': """
+       Module for marketing data""",
+
+    'description': """
+    This module handle the data coming from the landings
+    """,
+
+    'author': "David Heredia",
+    'website': "http://www.monitoringinnovation.com",
+
+    'version': '1.0',
+    'data': [
+        'views/leads.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
